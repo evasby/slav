a4fe7c7264e9f25295b18a8e970698fca84b96e5
Vladimir Dmitriev <defurator@mail.ru>
2015-03-02 21:40:19 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,124 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+  <title>Slavichi</title>
+  <link rel="stylesheet" type="text/css" href="css/all.css">
+  <script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
+  <!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+  <!--[if lte IE 8]>
+    <link rel="stylesheet" href="css/all-old-ie.css">
+  <![endif]-->
+  <!--[if gt IE 8]>
+    <link rel="stylesheet" href="css/all.css">
+  <![endif]-->
+  <!--[if lt IE 10]> 
+  <script type="text/javascript" src="js/jquery.placeholder.js"></script>
+  <![endif]-->
+</head>
+<body>
+  <header class="header">
+    <div class="inner">
+      <div class="header_top">
+        <div class="header_slang">Офисы и торговые помещения в самом центре минска!<span>Место имеет значение!</span></div>
+        <div class="header_logo"><a class="header_link" href="/">Славичи</a></div>
+        <div class="header_phone"><span></span>+375 (17) 289 90 92<br>+375 (29) 658 62 58<a href="/">Заказать звонок</a></div>
+      </div>
+      <nav class="menu">
+        <ul class="menu_list">
+          <li class="menu_item">
+            <a href="#" class="menu_link">Главная</a>
+          </li>
+          <li class="menu_item">
+            <a href="#" class="menu_link">О компании</a>
+          </li>
+          <li class="menu_item">
+            <a href="#" class="menu_link">Продажа офисов и торговых площадей</a>
+            <ul class="menu_list menu_list__l2">
+              <li class="menu_item menu_item__l2">
+                <a href="#" class="menu_link menu_link__l2">Офисная часть</a>
+              </li>
+              <li class="menu_item menu_item__l2">
+                <a href="#" class="menu_link menu_link__l2">Торговая и административная часть</a>
+              </li>
+              <li class="menu_item menu_item__l2">
+                <a href="#" class="menu_link menu_link__l2">Ресторан</a>
+              </li>
+            </ul>
+          </li>
+          <li class="menu_item">
+            <a href="#" class="menu_link">Как мы работаем</a>
+          </li>
+          <li class="menu_item">
+            <a href="#" class="menu_link">Партнеры</a>
+          </li>
+          <li class="menu_item">
+            <a href="#" class="menu_link">Новости</a>
+          </li>
+          <li class="menu_item">
+            <a href="#" class="menu_link">Контакты</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+  <div class="inner">
+    <article class="article">
+      <div class="slider">
+        <img src="image/slide1.jpg" alt="">
+        <div class="slider_text">
+          <h1>Акция</h1>
+          <p>Приглашаем всех желающих принять участие в презентации нашего гостиничного комплекса «Меркур». Презентация состоится 01.02.2015 года,  с 9-00 до 16-00 в апартотеле «Комфорт» по адресу г.Минск, ул.Щорса, д.1, 3 й этаж. </p>
+          <p>Звоните, ответим на все вопросы 8-029-658-62-58,8-017-289-18-90</p>
+        </div>
+      </div>
+      <div class="title">
+        <h1>Компания "Славичи"</h1>
+        <div class="title_border"></div>
+      </div>
+      <div class="article_text">
+        <p>Инвестиционная компания “Славичи” ведет строительство гостиницы “MERCURE”**** с фитнес-центром, рестораном, баром на улице Торговой вдоль набережной реки Свислочь. Проектом также предусмотрено наличие торговых и административных помещений.</p>
+        <p>В наше время “Верхний город” и современная улица Торговая, как и в 18 веке до сих пор остается кварталом ярких людей. которые всегда были и остаются в центре красивой жизни</p>
+      </div>
+      <div class="gallery">
+        <div class="gallery_item">
+          <img src="image/1.jpg" alt="">
+        </div>
+        <div class="gallery_item">
+          <img src="image/2.jpg" alt="">
+        </div>
+        <div class="gallery_item">
+          <img src="image/3.jpg" alt="">
+        </div>
+        <div class="gallery_item">
+          <img src="image/4.jpg" alt="">
+        </div>
+      </div>
+      <div class="quote">
+        Создавая наш проект, мы решили, что у Вас тоже обязательно должен быть офис в центре событий, где виды вдохновляют опережая время. Офис, где хочется работать и днем, и ночью.<br>Мы с любовью и гордостью проектируем и строим НАШ квартал.<br>Мы работаем тоже в самом центре Минска.<br>Поэтому предлагаем и Вам присоединиться к нам, для того чтобы продолжать традиции роскошной жизни и развивать свой бизнес создавая будущее!
+      </div>
+    </article>
+  </div>
+  <footer class="footer">
+    <div class="footer_top">
+      <div class="footer_logo">
+        <a href="/">Славичи</a>
+      </div>
+      <div class="footer_contacts">
+        <div class="footer_title">Контакты:</div>
+        <div class="footer_info">
+          <div class="footer_phone"><div class="footer_icon"></div>+375 (17) 289 90 92<br>+375 (17) 289 18 90<br>+375 (29) 658 62 58</div>
+          <div class="footer_wrap">
+            <div class="footer_address"><div class="footer_icon"></div>г. Минск, ул. Забицкая, 9-42</div>
+            <div class="footer_skype"><div class="footer_icon"></div>Skype: Slavichi.Slavichi</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="footer_bottom">
+      <a href="https://webformat.by/" class="webformat">Студия "Веб Формат"</a>  
+    </div>
+  </footer>
+</body>
+</html>
